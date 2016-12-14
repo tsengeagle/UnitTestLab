dcdf7a1e0346ba81eca52e6dfd89e5a7222b46ad
tsengeagle <tsengeagle@gmail.com>
2016-12-14 08:22:02 +0800

    流暢的assert

---
+++ FluentAssertion/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace FluentAssertion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class MyTestClass
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            var actual = "test";
+            actual.Should().StartWith("t").And.Contain("es").And.HaveLength(4);
+            
+        }
+
+
+    }
+}
